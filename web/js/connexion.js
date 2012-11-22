64b2b81968d0d88fc48fe0008339af1e9d3df491
Etienne <etienne.kerinckx@gmail.com>
2012-11-22 14:56:11 +0100

    Implémentation de la page de connexion (coté client)

--- web/js/connexion.js
+++ web/js/connexion.js
@@ -1,3 +1,33 @@
-function on_submit()
+function onSubmit()
 {
+	if (validate())
+		$("#formIdentification").submit();
+}
+
+function clearErrors()
+{
+	// Effacer les messages d'erreur déjà présents, s'il y en a
+    $("#fieldNom").removeClass();
+    $("#fieldNom").addClass("control-group");
+    $("#fieldMdp").removeClass();
+    $("#fieldMdp").addClass("control-group");
+}
+
+function validate()
+{
+	var result = true;
+	
+    if ($("#controlNom input").val().length <= 0)
+    {
+        $("#fieldNom").addClass("error");
+        result = false;
+    }
+	
+    if ($("#controlMdp input").val().length <= 0)
+    {
+        $("#fieldMdp").addClass("error");
+        result = false;
+    }
+	
+	return result;
 }
