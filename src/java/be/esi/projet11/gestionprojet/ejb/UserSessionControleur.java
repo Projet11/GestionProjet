ce54725be99d211d32b3a492d1966bf22f702b42
Etienne <etienne.kerinckx@yahoo.com>
2012-11-26 16:28:02 +0100

    Mise à jour de la page de connexion
    Utilisation d'éléments JSF sur la page de connexion
    Ajout d'un contrôleur (managed bean)

---
+++ src/java/be/esi/projet11/gestionprojet/ejb/UserSessionControleur.java
@@ -0,0 +1,60 @@
+package be.esi.projet11.gestionprojet.ejb;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author etienne
+ */
+@ManagedBean
+@SessionScoped
+public class UserSessionControleur
+{
+	private String inputNom;
+	private String inputPassword;
+	private boolean identificationEchouee;
+
+	@PostConstruct
+	public void init()
+	{
+		this.setIdentificationEchouee(false);
+	}
+	
+	public String getInputNom()
+	{
+		return this.inputNom;
+	}
+
+	public void setInputNom(String inputNom)
+	{
+		this.inputNom = inputNom;
+	}
+
+	public String getInputPassword()
+	{
+		return this.inputPassword;
+	}
+
+	public void setInputPassword(String inputPassword)
+	{
+		this.inputPassword = inputPassword;
+	}
+
+	public boolean isIdentificationEchouee()
+	{
+		return identificationEchouee;
+	}
+
+	public void setIdentificationEchouee(boolean identificationEchouee)
+	{
+		this.identificationEchouee = identificationEchouee;
+	}
+	
+	public String identifier()
+	{
+		this.setIdentificationEchouee(true);
+		return null;
+	}
+}
